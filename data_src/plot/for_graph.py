1b91dbdf22e70e1b7e16436ab10fe935fcbbcc03
sidthakur08 <41535376+sidthakur08@users.noreply.github.com>
2020-07-12 20:36:01 +0530

    pushed perc and doj
    functions to create dataframe with the percentile and date of joining

---
+++ data_src/plot/for_graph.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import datetime
+import plotly.express as px
+
+def create_perc(data):
+    perc = {'0-10':0,'10-20':0,'20-30':0,'30-40':0,'40-50':0,'50-60':0,'60-70':0,'70-80':0,'80-90':0,'90-100':0}
+    score = data['overall_score']
+
+    for i in score:
+        if ((i >= np.percentile(score,0))&(i < np.percentile(score,10))):
+            perc['0-10'] += 1
+        elif ((i >= np.percentile(score,10))&(i < np.percentile(score,20))):
+            perc['10-20'] += 1
+        elif ((i >= np.percentile(score,20))&(i < np.percentile(score,30))):
+            perc['20-30'] += 1
+        elif ((i >= np.percentile(score,30))&(i < np.percentile(score,40))):
+            perc['30-40'] += 1
+        elif ((i >= np.percentile(score,40))&(i < np.percentile(score,50))):
+            perc['40-50'] += 1
+        elif ((i >= np.percentile(score,50))&(i < np.percentile(score,60))):
+            perc['50-60'] += 1
+        elif ((i >= np.percentile(score,60))&(i < np.percentile(score,70))):
+            perc['60-70'] += 1
+        elif ((i >= np.percentile(score,70))&(i < np.percentile(score,80))):
+            perc['70-80'] += 1
+        elif ((i >= np.percentile(score,80))&(i < np.percentile(score,90))):
+            perc['80-90'] += 1
+        elif ((i >= np.percentile(score,90))&(i < np.percentile(score,100))):
+            perc['90-100'] += 1
+
+    data_perc = pd.DataFrame.from_dict(perc,orient='index',columns=['count'])
+    return data_perc
+
+def create_doj(data):
+    doj = {'this_week':0,'next_week':0,'this_month':0,'next_month':0}
+    dates = data['Date_Of_Joining']
+
+    for d in dates:
+        d_obj = datetime.datetime.strptime(d,'%Y-%m-%d %H:%M:%S.%f').date()
+        today = datetime.datetime.now().date()
+        diff = (d_obj - today).days
+
+        if ((diff>=0) & (diff<7)):
+            doj['this_week'] += 1
+        elif ((diff>=7) & (diff<14)):
+            doj['next_week'] += 1
+        elif ((today.month == d_obj.month) & (diff>=14)):
+            doj['this_month'] += 1
+        elif ((today.month != d_obj.month) & (diff>=14)):
+            doj['next_month'] += 1
+
+    data_doj = pd.DataFrame.from_dict(doj,orient='index',columns=['num_days'])
+
+    return data_doj
